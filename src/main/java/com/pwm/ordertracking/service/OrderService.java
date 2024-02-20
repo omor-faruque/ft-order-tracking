9f384f33ab6062b8cb2186fe409cdb5158c6256f
MdOmor Faruque <mdomor.faruque@rci.rogers.com>
2024-02-20 16:31:43 -0500

    updated

--- src/main/java/com/pwm/ordertracking/service/OrderService.java
+++ src/main/java/com/pwm/ordertracking/service/OrderService.java
@@ -5,34 +5,49 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pwm.ordertracking.model.Order;
+import com.pwm.ordertracking.model.OrderStatus;
 import com.pwm.ordertracking.repository.OrderRepository;
-
 
 @Service
 public class OrderService {
 	private final OrderRepository orderRepository;
-	
+
 	@Autowired
 	public OrderService(OrderRepository orderRepository) {
 		this.orderRepository = orderRepository;
 	}
-	
+
 	public List<Order> getOrders() {
 		return orderRepository.findAll();
 	}
-	
+
 	public Order createOrder(Order order) {
+		
 		Order orderEntity = new Order();
 		orderEntity.setCustomerName(order.getCustomerName());
 		orderEntity.setSourceOrderId(order.getSourceOrderId());
 		orderEntity.setShippingAddress(order.getShippingAddress());
-		orderEntity.setOrderStatus(order.getOrderStatus());
+		if (isValidOrderStatus(order.getOrderStatus())) {
+			orderEntity.setOrderStatus(order.getOrderStatus());
+		}
+		else {
+			orderEntity.setOrderStatus(OrderStatus.PROCESSING);
+		}
+		
 		return orderRepository.save(orderEntity);
 	}
-	
+
 	public void deleteOrderById(Long id) {
 		this.orderRepository.deleteById(id);
 	}
-	
+
+	private boolean isValidOrderStatus(OrderStatus status) {
+		for (OrderStatus os : OrderStatus.values()) {
+			if (os.equals(status)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 }
